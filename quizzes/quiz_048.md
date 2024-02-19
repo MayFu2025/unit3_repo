7fbae866424401ad3c20877c16197948d4b51a24
May <2025.may.fujita@uwcisak.jp>
2024-02-19 11:46:37 +0900

    quizzes cont.

--- quizzes/quiz_048.md
+++ quizzes/quiz_048.md
@@ -7,7 +7,19 @@
 
 ### Solution
 ```.py
+from quiz_lib import DatabaseWorker, check_text, make_hash
 
+db = DatabaseWorker(name='bitcoin_exchange.db')
+query = "Select * from ledger"
+result = db.search(query=query, multiple=True)
+
+total = 0
+for n in result:
+    pre_hash = f"id {n[0]},sender_id {n[1]},receiver_id {n[2]},amount {n[3]}"
+    if check_text(hashed=n[4], text=pre_hash):
+        total += n[3]
+
+print(total)
 ```
 
 ### Evidence
--- quizzes/quiz_files/quiz_048.py
+++ quizzes/quiz_files/quiz_048.py
@@ -1,12 +1,13 @@
-from quiz_lib import DatabaseWorker, check_text
+from quiz_lib import DatabaseWorker, check_text, make_hash
 
 db = DatabaseWorker(name='bitcoin_exchange.db')
 query = "Select * from ledger"
 result = db.search(query=query, multiple=True)
 
+total = 0
 for n in result:
     pre_hash = f"id {n[0]},sender_id {n[1]},receiver_id {n[2]},amount {n[3]}"
     if check_text(hashed=n[4], text=pre_hash):
-        print(f"Tx(id={n[0]}) Signature Matches")
-    else:
-        print(f"Tx(id={n[0]}) Error Signature")
+        total += n[3]
+
+print(total)
