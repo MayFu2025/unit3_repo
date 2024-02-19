0efb581a2f02f696cea3f2b7184e2a15377b0e0b
May <2025.may.fujita@uwcisak.jp>
2024-02-19 09:05:47 +0900

    lesson on kivy droplist quiz 50, need to add 49 need to finish quiz documentation

--- quizzes/quiz_files/quiz_050.py
+++ quizzes/quiz_files/quiz_050.py
@@ -10,3 +10,11 @@ class Flight:
         output = f"{self.duration[0]} hours {self.duration[1]} minutes and {self.duration[2]} seconds"
         return output
 
+
+# Check that it works
+
+MaysFlight = Flight('ANA123', 'HND', 'NRT', "2024-02-19, 8:00AM", [1, 2, 3])
+print(MaysFlight.get_duration())
+
+BobsFlight = Flight('JAL501', 'NRT', 'KIX', "2024-05-01, 5:30AM", [16, 17, 18])
+print(BobsFlight.get_duration())
